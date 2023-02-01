ed9b120271a43fbe3b132e59df086521ba74ef1d
Xiaochen Lu <xiaoclu@ebay.com>
2023-02-01 17:47:18 +0800

    feat: basic increment/decrement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-function App() {
-  return (
-    <div>
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-    </div>
-  );
+function App() {
+    const dispatch = useDispatch();
+    const count = useSelector((state) => state.counter.value);
+
+    return (
+        <div>
+            <button
+                onClick={() =>
+                    dispatch({ type: "counter/increment", payload: null })
+                }
+            >
+                Increment
+            </button>
+            <div>{count}</div>
+            <button
+                onClick={() =>
+                    dispatch({ type: "counter/decrement", payload: null })
+                }
+            >
+                Decrement
+            </button>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import App from "./App";
+import React from "react";
+import store from "./app/store";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
